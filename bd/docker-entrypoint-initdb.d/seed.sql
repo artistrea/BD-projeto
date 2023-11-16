2cf39af8c84368210dcd0379bf8656bc266ec125
artistrea <apiravesi@gmail.com>
2023-11-16 01:01:17 -0300

    feat/emprestimos

--- bd/docker-entrypoint-initdb.d/seed.sql
+++ bd/docker-entrypoint-initdb.d/seed.sql
@@ -51,7 +51,8 @@ INSERT INTO "MateriaisDidaticos"
 
 -- Inserções por Miguel Carvalho de Medeiros ----------------------------------
 
-INSERT INTO "Items" ("id", "type", "descricao", "categoria", "dataDeAquisicao", "estadoDeConservacao", "localizacao", "uriImagem")
+INSERT INTO "Items"
+    ("id", "type", "descricao", "categoria", "dataDeAquisicao", "estadoDeConservacao", "localizacao", "uriImagem")
     VALUES
     (8577803828, 'livro', 'Livro sobre modelagem de Bancos de Dados. 6ª edição', 'Bancos de Dados', TO_DATE('12/11/2023', 'DD/MM/YYYY'), 'bom', 'prateleira 1', 'https://media.istockphoto.com/id/466564401/pt/vetorial/modelo-de-livro-vertical-em-branco.jpg?s=1024x1024&w=is&k=20&c=ZKSJ4qganGkiieYJMx6ODiCmZUuXLmdOmWBn_qg6Byc='),
     (8579360855, 'livro', 'Livro sobre sistemas de bancdos de dados', 'Bancos de Dados', TO_DATE('01/01/2001', 'DD/MM/YYYY'), 'ruim', 'prateleira 4', 'https://media.istockphoto.com/id/495477978/pt/foto/livro-aberto.jpg?s=2048x2048&w=is&k=20&c=dnNS2jDfWlODq7CtV_IyAdZCaeLKEb0P1mpMFzfkwPM='),
@@ -65,3 +66,20 @@ INSERT INTO "Livros" ("ISBN", "title", "author")
 
 ---------------------------------------------------------------
 
+-- Inserções por Samuel Carvalho ----------------------------------
+
+INSERT INTO "Emprestimos" 
+    ("dataDeEmprestimo", "dataDeDevolucaoPrevista", "status", "userId", "itemId", "itemType")
+    VALUES
+    ('2023-04-01', '2023-04-08', 'concluido', 1, 101507, 'materialDidatico'),
+    ('2023-05-01', '2023-05-08', 'concluido', 2, 8577803828, 'livro'),
+    ('2023-06-01', '2023-06-08', 'concluido', 3, 101507, 'materialDidatico'),
+    ('2023-07-01', '2023-07-08', 'concluido', 1, 8577803828, 'livro'),
+    ('2023-08-01', '2023-08-08', 'concluido', 2, 101507, 'materialDidatico'),
+    ('2023-09-01', '2023-09-08', 'concluido', 3, 8577803828, 'livro'),
+    ('2023-10-01', '2023-10-08', 'emAndamento', 1, 101507, 'materialDidatico'),
+    ('2023-11-01', '2023-11-08', 'emAndamento', 2, 8577803828, 'livro'),
+    ('2023-12-01', '2023-12-08', 'emAndamento', 3, 566542, 'materialDidatico'),
+    ('2024-01-01', '2024-01-08', 'emAndamento', 1, 1234567890, 'livro');
+
+---------------------------------------------------------------
