0957cb8a4bc1788f2d6eae4e514e70c3a6399b4b
Pedro <pedroximenes76@gmail.com>
2023-12-13 15:28:51 -0300

    Adicionando funcionalidade no botão Sair

--- frontend/src/layouts/NavbarLayout.svelte
+++ frontend/src/layouts/NavbarLayout.svelte
@@ -1,6 +1,21 @@
 <script>
+    import { user } from "../stores/user";
     export let page;
     export let params;
+
+    import { logout } from "../stores/user";
+  	import router from "page";
+
+    function logoutAccount() {
+		  logout().then(() => {
+        router.redirect("/login");
+        alert("Logout com sucesso!")
+      })
+      .catch(err => {
+        if (err.response.status === 401) alert("Nenhum usuário está logado!")
+        else alert("Ocorreu um erro inesperado no servidor!")
+      })
+	}
 </script>
 
 
@@ -13,7 +28,7 @@
       <a class="block py-2 px-4 hover:bg-background transition-colors" href="/inventario">Inventário</a>
     </li>
     <li>
-      <button class="block py-2 px-4 hover:bg-background transition-colors">Sair</button>
+      <button on:click={logoutAccount} class="block py-2 px-4 hover:bg-background transition-colors">Sair</button>
     </li>
   </ul>
 </nav>  
